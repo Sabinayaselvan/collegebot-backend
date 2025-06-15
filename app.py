e45eb89935979c216ea90b5d6b4c8e000014f4b4
Sabinayaselvan <sabinayamaheswari@gmail.com>
2025-06-15 18:08:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -145,4 +145,6 @@ def health_check():
 
 # === Main ===
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
+
